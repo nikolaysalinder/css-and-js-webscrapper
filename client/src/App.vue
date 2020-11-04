1bc7eda742098884a145a0358a4d901d1a5ad43d
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-04 16:33:20 +0500

    Finish work

---
+++ client/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <form @submit.prevent="getScripts">
+      <label for="site"
+        >Введите адрес сайта в формате https://www.vk.com
+      </label>
+      <br />
+      <input id="site" type="text" v-model="site" />
+      <br />
+      <input type="submit" value="Получить скрипты" />
+    </form>
+    <div class="results">
+      <p v-bind:key="index" v-for="(result, index) in results" class="scripts">
+        {{ `${index + 1}) ${result}` }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      site: "https://www.facebook.com/",
+      results: [],
+    };
+  },
+  methods: {
+    getScripts() {
+      axios
+        .post("http://localhost:3000/scripts", {
+          site: this.site,
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.results = response.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#site {
+  padding: 7px;
+  min-width: 250px;
+}
+.results {
+  text-align: left;
+  margin-bottom: 20px;
+}
+</style>
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const Koa = require("koa");
+const cors = require("@koa/cors");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const puppeteer = require("puppeteer");
+
+const app = new Koa();
+
+app.use(cors());
+
+app.use(bodyParser());
+
+const router = new Router();
+
+router.post("/scripts", async (ctx, next) => {
+  console.log(ctx.request.body);
+
+  const domain = ctx.request.body.site;
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(domain).catch((err) => console.log(err));
+  await page.waitFor(1000);
+
+  const scripts = await page.evaluate(() => {
+    let result = [];
+    let scripts = document.getElementsByTagName("script");
+    for (let script of scripts) {
+      if (script.getAttribute("src")) {
+        let src = script.getAttribute("src");
+        result.push(src);
+      }
+    }
+    let links = document.getElementsByTagName("link");
+    for (let link of links) {
+      if (link.getAttribute("type") == "text/css") {
+        result.push(link.getAttribute("href"));
+      }
+    }
+    return result;
+  });
+
+  browser.close();
+  ctx.body = scripts;
+  await next();
+});
+
+app.use(router.routes());
+
+app.listen(3000, () => {
+  console.log("App is running on http://localhost:3000");
+});
