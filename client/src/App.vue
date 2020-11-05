3adf7529c1652a8519c342ce76a25b01807a0406
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-05 10:42:00 +0500

    Added spinner

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,9 +8,17 @@
       <br />
       <input id="site" type="text" v-model="site" />
       <br />
-      <input type="submit" value="Получить скрипты" />
+      <button type="submit">Получить скрипты</button>
     </form>
     <div class="results">
+      <div v-if="isLoading" class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+      </div>
       <p v-bind:key="index" v-for="(result, index) in results" class="scripts">
         {{ `${index + 1}) ${result}` }}
       </p>
@@ -26,17 +34,20 @@ export default {
   data() {
     return {
       site: "https://www.facebook.com/",
+      isLoading: false,
       results: [],
     };
   },
   methods: {
     getScripts() {
+      this.isLoading = true;
       axios
         .post("http://localhost:3000/scripts", {
           site: this.site,
         })
         .then((response) => {
           console.log(response.data);
+          this.isLoading = false;
           this.results = response.data;
         })
         .catch((err) => console.log(err));
@@ -63,5 +74,92 @@ export default {
 }
 .results {
   text-align: left;
+  position: relative;
+}
+.sk-chase {
+  width: 40px;
+  height: 40px;
+  position: relative;
+  animation: sk-chase 2.5s infinite linear both;
+  margin: 30px auto;
+}
+
+.sk-chase-dot {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  animation: sk-chase-dot 2s infinite ease-in-out both;
+}
+
+.sk-chase-dot:before {
+  content: "";
+  display: block;
+  width: 25%;
+  height: 25%;
+  background-color: #000;
+  border-radius: 100%;
+  animation: sk-chase-dot-before 2s infinite ease-in-out both;
+}
+
+.sk-chase-dot:nth-child(1) {
+  animation-delay: -1.1s;
+}
+.sk-chase-dot:nth-child(2) {
+  animation-delay: -1s;
+}
+.sk-chase-dot:nth-child(3) {
+  animation-delay: -0.9s;
+}
+.sk-chase-dot:nth-child(4) {
+  animation-delay: -0.8s;
+}
+.sk-chase-dot:nth-child(5) {
+  animation-delay: -0.7s;
+}
+.sk-chase-dot:nth-child(6) {
+  animation-delay: -0.6s;
+}
+.sk-chase-dot:nth-child(1):before {
+  animation-delay: -1.1s;
+}
+.sk-chase-dot:nth-child(2):before {
+  animation-delay: -1s;
+}
+.sk-chase-dot:nth-child(3):before {
+  animation-delay: -0.9s;
+}
+.sk-chase-dot:nth-child(4):before {
+  animation-delay: -0.8s;
+}
+.sk-chase-dot:nth-child(5):before {
+  animation-delay: -0.7s;
+}
+.sk-chase-dot:nth-child(6):before {
+  animation-delay: -0.6s;
+}
+
+@keyframes sk-chase {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes sk-chase-dot {
+  80%,
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes sk-chase-dot-before {
+  50% {
+    transform: scale(0.4);
+  }
+  100%,
+  0% {
+    transform: scale(1);
+  }
 }
 </style>
